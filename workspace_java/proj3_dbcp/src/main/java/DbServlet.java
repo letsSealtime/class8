2b6aed9cf6912accfd6c3c0f58431186b2bcac7e
DESKTOP-ETJ58KB\admin <dy8426@gmail.com>
2025-02-27 17:31:06 +0900

    0227

--- workspace_java/proj3_dbcp/src/main/java/DbServlet.java
+++ workspace_java/proj3_dbcp/src/main/java/DbServlet.java
@@ -1,6 +1,7 @@
 
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.Date;
 import java.sql.PreparedStatement;
@@ -21,76 +22,90 @@ import javax.sql.DataSource;
 
 import kr.or.human.dto.EmpDTO;
 
-@WebServlet("/DbServlet")
+@WebServlet("/db")
 public class DbServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
-
+    
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		System.out.println("/db doGet 실행");
-	
 		
 		try {
-			// DB 접속 시작
-			// context.xml의 Resource 중의 jdbc/oracle 가져오기
-			// JNDI 방식으로. 
 			
+			// [DB 접속] 시작
+			// context.xml의 Resource 중에 jdbc/oracle 가져오기
+			// JNDI 방식으로...
 			Context ctx = new InitialContext();
 			DataSource ds = (DataSource) ctx.lookup("java:/comp/env/jdbc/oracle");
-			
 			// 커넥션 풀에서 접속 정보를 가져오기
-			// 접속이 안 되면 null이 나온다.
+			// 접속이 안되면 null
 			Connection con = ds.getConnection();
 			
-			// DB 접속 끝
-			
-			// SQL 준비
-			String query = "select * from emp";
+			// [SQL 준비]
+			String query = " select * from";	//select * fromemp
+			query += " emp";
 			PreparedStatement ps = con.prepareStatement(query);
 			
 			// [SQL 실행] 및 [결과 확보]
-			//		ResultSet executeQuery : SQL 중 select 실행
-			// 		int executeUpdate : select 외 모든 것
+			// 		ResultSet executeQuery() : SQL 중 select 실행
+			//		int executeUpdate() : select 외 모든 것
 			//		ResultSet : select 조회 결과 전체 : 엑셀 테이블 느낌
 			ResultSet rs = ps.executeQuery();
 			
-			
-			// 결과 활용 단계
-//			rs.next();		// 다음 행으로 커서를 이동(첫번째 행)
+			// 결과 활용
+//			rs.next();     	// 다음 줄로 커서를 이동
 							// 다음 줄이 있으면 true, 없으면 false를 return
 			
-			List list = new ArrayList();
-			while (rs.next()) { // 다음줄이 없을 때까지 반복한다.
+//			List<Map> list = new ArrayList<Map>();
+			List<EmpDTO> list = new ArrayList<EmpDTO>();
+			while( rs.next() ) {	// 다음 줄이 없을 때 까지 반복
 				int empno = rs.getInt("empno");
 				String ename = rs.getString("ename");
 				Date hiredate = rs.getDate("hiredate");
 				System.out.println("----------");
-				System.out.print(" empno : " + empno);
-				System.out.print(" ename : " + ename);
-				System.out.print(" hiredate : " + hiredate);
+				System.out.print(" empno : "+ empno);
+				System.out.print(" ename : "+ ename);
+				System.out.print(" hiredate : "+ hiredate);
 				System.out.println();
 				
 				Map map = new HashMap();
 				map.put("empno", empno);
-				map.put("ename", ename);
+//				map.put("ename", ename);
+				map.put("사원명", ename);
 				map.put("hiredate", hiredate);
+				map.put("hehe", 123);
 				
 				EmpDTO empDTO = new EmpDTO();
 				empDTO.setEmpno(empno);
 				empDTO.setEname(ename);
 				empDTO.setHiredate(hiredate);
 				
+//				list.add(map);
 				list.add(empDTO);
-				
-				
-				
 			}
 			
+			// View
+			PrintWriter out = response.getWriter();
+			out.println("<table border='1'>"); 
+			out.println("    <tr>");
+			out.println("        <th>empno</th>");
+			out.println("        <th>ename</th>");
+			out.println("        <th>hiredate</th>");
+			out.println("    </tr>");
+			
+			for(int i=0; i<list.size(); i++) {
+				EmpDTO dto = list.get(i);
+				out.println("    <tr>");
+				out.println("        <td>"+ dto.getEmpno() +"</td>");
+				out.println("        <td>"+ dto.getEname() +"</td>");
+				out.println("        <td>"+ dto.getHiredate() +"</td>");
+				out.println("    </tr>");
+			}
+			out.println("</table>");
+			
+			// 커넥션풀로 반환
+			con.close();
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
-	
 	}
-
 
 }
---
+++ workspace_java/proj4_forward/src/main/webapp/header.jsp
@@ -0,0 +1,21 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+
+<header>
+	<div>이거 헤더임</div>
+	<div>로고</div>
+	<div>누구님 환영합니다.</div>
+</header>
+
+<!-- jsp에서 직접 text에 1234라는 값을 보내고, 
+jsp에서 html로 그 값을 출력하시오.-->
+
+</body>
+</html>
